16d7f199a26aa189573eea62ee9883d5b442f286
LPshallot-travail <clxt.pct@outlook.com>
2024-10-05 16:16:04 +0200

    dernier ajout avant le premier test

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'test_location');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '!*64yRHFaB*8M6fCb');
+
+// Autres paramètres de configuration
+?>
